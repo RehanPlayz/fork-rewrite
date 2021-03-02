a678f63b90a4522a25d068052ee93bfcf154fc18
ExoticalCat <itznya10@gmail.com>
2021-03-02 21:14:35 +0200

    relrease

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,37 @@
-FROM debian:10.1-slim
+FROM quay.io/jitesoft/debian:buster-slim
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update
-RUN apt-get -y install apt-utils curl software-properties-common apt-transport-https ca-certificates wget dirmngr gnupg iproute2 libopus0 make g++ locales git cmake zip unzip 
-RUN addgroup --gid 998 container 
-RUN useradd -r -u 999 -d /home/container -g container -m container
-
+RUN apt-get update && apt-get upgrade -y \
+    && apt-get -y install iptables apt-utils curl ffmpeg software-properties-common apt-transport-https ca-certificates wget dirmngr gnupg iproute2 youtube-dl sqlite3 libopus0 make g++ locales git cmake zip unzip libtool-bin autoconf automake curl jq
+    
+## User 
+RUN addgroup --gid 998 container
+RUN useradd -m -u 999 -d /home/container -g container -s /bin/bash container
+  
     # Ensure UTF-8
-RUN locale-gen en_US.UTF-8
-ENV LANG en_US.UTF-8
 ENV TZ=UTC
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-    # Java 8
-RUN mkdir /usr/share/man/man1 && wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add - && add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ 
-RUN apt-get update && apt-get -y install adoptopenjdk-11-hotspot
-RUN java -version
+   # Fixes
+RUN apt-get install -y gcc g++ libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev libgcc1 lib32gcc1 gdb libc6 git wget tar zip unzip binutils xz-utils liblzo2-2 iproute2 net-tools netcat telnet libatomic1 libsdl1.2debian libsdl2-2.0-0 libfontconfig libicu63 icu-devtools libunwind8 libssl-dev sqlite3 libmariadbclient-dev locales openssl libc6-dev libstdc++6 libssl1.1 lib32stdc++6 g++ make libirrlicht-dev cmake libpng-dev libjpeg-dev libxxf86vm-dev libgl1-mesa-dev libogg-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev libgmp-dev libjsoncpp-dev dnsutils python3 build-essential zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl llvm libncurses5-dev libncursesw5-dev tk-dev libffi-dev liblzma-dev python-openssl libespeak-dev espeak sox libsox-fmt-mp3 libespeak1 libssl-dev less libasound2 libegl1-mesa libglib2.0-0 libnss3 libpci3 libpulse0 libxcursor1 libxslt1.1 libx11-xcb1 libxkbcommon0 locales pulseaudio python x11vnc x11-xkb-utils
+
+  # Font 
+RUN update-locale lang=en_US.UTF-8 \
+ && dpkg-reconfigure --frontend noninteractive locales
+
+   # yarn
+RUN apt-get update \
+    && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+    && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
+    && apt-get update && apt-get -y install yarn 
+
+
+    # Pyenv && Python    
+RUN apt-get update && apt-get -y install libffi-dev     
+
+  # Others
+RUN apt-get update && apt-get install -y rpl libx11-dev libxkbfile-dev libsecret-1-dev
 
 USER container
 ENV  USER container
@@ -24,7 +39,6 @@ ENV  HOME /home/container
 
 WORKDIR /home/container
 
-COPY ./start.sh /start.sh
 COPY ./entrypoint.sh /entrypoint.sh
 
 CMD ["/bin/bash", "/entrypoint.sh"]
--- start.sh
+++ start.sh
@@ -1,3 +1,401 @@
 #!/bin/bash
-$STARTUP_CMD 
-$SECOND_CMD
+
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
+go_env(){
+  # Goenv
+  
+if [ -d "temp-folder" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p temp-folder
+fi
+  
+if [ -z "$GOLANG_VERSION" ] ; then
+    echo -e "==============================="
+    echo -e "Golang version can be 1.12.1 , 1.13.1 , 1.14.1 , 1.15.1 , custom"
+    read -p "Golang: " GOLANG_VERSION_SELECT
+    echo -e "==============================="
+case $GOLANG_VERSION_SELECT in
+1.12.1)
+  echo -e "==============================="
+  echo -e "Golang 1.12.1"
+  GOLANG_VERSION="1.12.1"
+  echo -e "==============================="
+;;
+1.13.1)
+  echo -e "==============================="
+  echo -e "Golang 1.13.1"
+  GOLANG_VERSION="1.13.1"
+  echo -e "==============================="
+;;
+1.14.1)
+  echo -e "==============================="
+  echo -e "Golang 1.14.1"
+  GOLANG_VERSION="1.14.1"
+  echo -e "==============================="
+;;
+1.15.1)
+  echo -e "==============================="
+  echo -e "Golang 3.8.7"
+  GOLANG_VERSION="3.8.7"
+  echo -e "==============================="  
+;;
+custom)
+  echo -e "==============================="
+  echo -e "Custom Golang version"
+  read -p "Golang version: " GOLANG_VERSION
+  echo -e "Using Golang $GOLANG_VERSION"
+  echo -e "==============================="
+;;
+*)
+  echo -e "hi" 2> /dev/null > /dev/null
+;;
+esac
+fi
+
+git clone https://github.com/syndbg/goenv.git ~/.goenv 2> /dev/null > /dev/null
+export GOENV_ROOT="$HOME/.goenv"
+export PATH="$GOENV_ROOT/bin:$PATH"
+export PATH="$GOROOT/bin:$PATH"
+export PATH="$PATH:$GOPATH/bin"
+eval "$(goenv init -)" 
+export GOPATH="$HOME/go" >> ~/.bash_profile
+export TMPDIR="$HOME/temp-folder"
+     sleep 5s
+     echo -e "==============================="
+     echo -e "Available Golang versions: $(goenv install --list)"
+     echo -e "==============================="
+     sleep 5s
+     echo -e "Using Golang ${GOLANG_VERSION}"
+     goenv install "${GOLANG_VERSION}" -s
+     goenv global "${GOLANG_VERSION}"
+     rm -rf temp-folder/go-build*
+}
+
+py_env(){
+  # Pyenv
+
+if [ -d "temp-folder" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p temp-folder
+fi
+
+if [ -z "$PYTHON_VERSION" ] ; then
+    echo -e "==============================="
+    echo -e "Python version can be 3.5.10 , 3.6.12 , 3.7.9 , 3.8.7 , 3.9.1 , custom"
+    read -p "Python: " PYTHON_VERSION_SELECT
+    echo -e "==============================="
+case $PYTHON_VERSION_SELECT in
+3.5.10)
+  echo -e "==============================="
+  echo -e "Python 3.5.10"
+  PYTHON_VERSION="3.5.10"
+  echo -e "==============================="
+;;
+3.6.12)
+  echo -e "==============================="
+  echo -e "Python 3.6.12"
+  PYTHON_VERSION="3.6.12"
+  echo -e "==============================="
+;;
+3.7.9)
+  echo -e "==============================="
+  echo -e "Python 3.7.9"
+  PYTHON_VERSION="3.7.9"
+  echo -e "==============================="
+;;
+3.8.7)
+  echo -e "==============================="
+  echo -e "Python 3.8.7"
+  PYTHON_VERSION="3.8.7"
+  echo -e "==============================="  
+;;
+3.9.1)
+  echo -e "==============================="
+  echo -e "Python 3.9.1"
+  PYTHON_VERSION="3.9.1"
+  echo -e "==============================="
+;;
+custom)
+  echo -e "==============================="
+  echo -e "Custom python version"
+  read -p "Python version: " PYTHON_VERSION
+  echo -e "Using python $PYTHON_VERSION"
+  echo -e "==============================="
+;;
+*)
+  echo -e "hi" 2> /dev/null > /dev/null
+;;
+esac
+fi
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv 2> /dev/null > /dev/null
+export PYENV_ROOT="$HOME/.pyenv"
+PATH="/home/container/.pyenv/bin:$PATH"
+eval "$(pyenv init -)" 2> /dev/null > /dev/null
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv 2> /dev/null > /dev/null
+eval "$(pyenv virtualenv-init -)" 2> /dev/null > /dev/null
+export TMPDIR="$HOME/temp-folder"
+     sleep 5s
+     echo -e "==============================="
+     echo -e "Available Python versions: $(pyenv install --list)"
+     echo -e "==============================="
+     sleep 5s
+     echo -e "Using Python ${PYTHON_VERSION}"
+     pyenv install "${PYTHON_VERSION}" -s
+     pyenv global "${PYTHON_VERSION}"
+     rm -rf temp-folder/python-build*
+}
+
+jabba_java(){
+# Jabba
+
+if [ -d "temp-folder" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p temp-folder
+fi
+
+if [ -z "$JAVA_VERSION" ] ; then
+    echo -e "==============================="
+    echo -e "Java version can be java8 , java11 , latest , custom"
+    read -p "Java: " JAVA_VERSION_SELECT
+    echo -e "==============================="
+case $JAVA_VERSION_SELECT in
+java8)
+  echo -e "==============================="
+  echo -e "Java 8"
+  JAVA_VERSION="adopt@1.8-0"
+  echo -e "==============================="
+;;
+java11)
+  echo -e "==============================="
+  echo -e "Java 11"
+  JAVA_VERSION="adopt@1.11-0"
+  echo -e "==============================="
+;;
+latest)
+  echo -e "==============================="
+  echo -e "Java 15"
+  JAVA_VERSION="adopt@1.15-0"
+  echo -e "==============================="
+;;
+custom)
+  echo -e "==============================="
+  echo -e "Custom java version"
+  read -p "Java version: " JAVA_VERSION
+  echo -e "Using java $JAVA_VERSION"
+  echo -e "==============================="
+;;
+*)
+  echo -e "hi" 2> /dev/null > /dev/null
+;;
+esac
+fi
+
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh 2> /dev/null > /dev/null
+[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh" 2> /dev/null > /dev/null
+export TMPDIR="$HOME/temp-folder"
+     sleep 5s
+     echo -e "==============================="
+     echo -e "Available Java versions: $(jabba ls-remote)"
+     echo -e "==============================="
+     sleep 5s
+     jabba install "${JAVA_VERSION}"
+     jabba use "${JAVA_VERSION}"
+     jabba alias default "${JAVA_VERSION}"
+     echo -e "==============================="
+     java -version
+     echo -e "==============================="
+}
+
+nvm_nodejs(){
+# NVM
+
+if [ -e ".nvmrc" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    touch .nvmrc
+fi
+
+if [ -d ".nvm" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p .nvm
+fi
+
+if [ -d ".npm-global" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p .npm-global
+fi
+
+if [ -d ".npm-global/lib" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p .npm-global/lib
+fi
+
+if [ -d "temp-folder" ]
+then 
+    echo -e "HI" 2> /dev/null > /dev/null
+else
+    mkdir -p temp-folder
+fi
+
+if [ -z "$NODEJS_VERSION" ] ; then
+    echo -e "==============================="
+    echo -e "NodeJS version can be nodejs8 , nodejs10 , nodejs12 , nodejs14 , nodejs15 , custom"
+    read -p "NodeJS: " NODEJS_VERSION_SELECT
+    echo -e "==============================="
+case $NODEJS_VERSION_SELECT in
+nodejs8)
+  echo -e "==============================="
+  echo -e "NodeJS 8"
+  NODEJS_VERSION="8"
+  echo -e "==============================="
+;;
+nodejs10)
+  echo -e "==============================="
+  echo -e "NodeJS 10"
+  NODEJS_VERSION="10"
+  echo -e "==============================="
+;;
+nodejs12)
+  echo -e "==============================="
+  echo -e "NodeJS 12"
+  NODEJS_VERSION="12"
+  echo -e "==============================="
+;;
+nodejs14)
+  echo -e "==============================="
+  echo -e "NodeJS 14"
+  NODEJS_VERSION="14"
+  echo -e "==============================="  
+;;
+nodejs15)
+  echo -e "==============================="
+  echo -e "NodeJS 15"
+  NODEJS_VERSION="15"
+  echo -e "==============================="
+;;
+custom)
+  echo -e "==============================="
+  echo -e "Custom NodeJS version"
+  read -p "NodeJS version: " NODEJS_VERSION
+  echo -e "Using NodeJS $NODEJS_VERSION"
+  echo -e "==============================="
+;;
+*)
+  echo -e "hi" 2> /dev/null > /dev/null
+;;
+esac
+fi
+
+curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | NVM_DIR=/home/container/.nvm bash 2> /dev/null > /dev/null
+NODE_PATH=$NVM_DIR/v$NODE_VERSION/lib/node_modules 
+export NVM_DIR="/home/container/.nvm" 2> /dev/null > /dev/null
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm 2> /dev/null > /dev/null
+     sleep 3s
+     echo -e "==============================="
+     echo -e "Available NodeJS versions: $(nvm ls-remote)"
+     echo -e "==============================="
+     sleep 2s
+     echo -e "==============================="
+     echo -e "Installing NodeJS ${NODEJS_VERSION}" 
+     nvm install "${NODEJS_VERSION}"
+     nvm alias default "${NODEJS_VERSION}"
+     echo -e "==============================="
+     sleep 2s
+     echo -e "==============================="
+     echo -e "Configuring Global packages location"
+     npm config set prefix '~/.npm-global'
+     export PATH=~/.npm-global/bin:$PATH
+     echo -e "==============================="
+     sleep 2s
+     echo -e "==============================="
+     echo -e "Upgrading NPM to latest version"
+     npm install -g npm@latest
+     echo -e "==============================="
+ 
+}
+
+bot_startup(){
+
+case $SERVER_TYPE in
+python)
+  py_env
+  ${STARTUP_CMD}
+;;
+nodejs)
+  nvm_nodejs
+  ${STARTUP_CMD}
+;;
+java)
+  jabba_java
+  ${STARTUP_CMD}
+
+;;
+golang)
+  go_env
+  ${STARTUP_CMD}
+;;
+*)
+  echo -e "hi" 2> /dev/null > /dev/null
+;;
+esac    
+
+if [ -e "backup.me" ]
+then 
+    echo -e "==============================="
+    echo -e "Make sure you have enough Disk Storage"
+    echo -e "==============================="
+    sleep 5s
+    zip -r "backup-$(date +"%Y-%m-%d %H-%M-%S").zip" *
+    echo -e "==============================="
+    echo -e "Backup Finished"
+    echo -e "Download Backup Archive from File Manager or SFTP"
+    echo -e "==============================="
+    sleep 5s
+    rm -rf backup.me
+    exit
+fi
+}
+
+startup(){
+    output "==============================="
+    output "| RAM: $SERVER_MEMORY MB"
+    output "| IP: $SERVER_IP:$SERVER_PORT"
+    output "| Server UUID: $(hostname)"
+    output "==============================="
+    output ""
+
+
+    output "==============================="
+    output "| Create backup.me file to backup server"
+    output "| Don't forget to backup server regularly"
+    output "==============================="
+    output ""
+    
+    output "==============================="
+    output "| Starting...."
+    output "| Don't forget to configure server type at variables"
+    output "==============================="
+    output ""
+
+}
+
+#Execution
+startup
+bot_startup
